7b61f697e7cbb0b5e17dbf96a61534072f27a20a
ashraful88 <aaj.cse@gmail.com>
2015-07-09 20:30:02 +0600

    installer
    Product Attr

--- app/code/local/AdminScript/Installer/data/adminscript_cms_setup/data-install-1.0.0.php
+++ app/code/local/AdminScript/Installer/data/adminscript_cms_setup/data-install-1.0.0.php
@@ -2,9 +2,10 @@
 $installer = $this;
 $installer->startSetup();
 
-// For creating a block for a store
+// For creating for a store
 //$store = Mage::app()->getStore('store_code');
 
+// ===== CMS Static Blocks =====
 $data = array(
     'title' => 'Hello World',
     'identifier' => 'hello-world-id',
@@ -20,5 +21,33 @@ if ($block->getId() == false) {
     // Create static block:
     Mage::getModel('cms/block')->setData($data)->save();
 }
+
+
+// ==== Product Attributes =====
+
+$model =  new Mage_Catalog_Model_Resource_Setup();
+$model->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'my_custom_attribute_code', array(
+    'group' => 'Prices',
+    'type' => 'text',
+    'attribute_set' =>  'Default', // Your custom Attribute set
+    'backend' => '',
+    'frontend' => '',
+    'label' => 'My Custom Attribute',
+    'input' => 'text',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible' => true,
+    'required' => false,
+    'user_defined' => true,
+    'default' => '1',
+    'searchable' => false,
+    'filterable' => true,
+    'comparable' => false,
+    'visible_on_front' => true,
+    'visible_in_advanced_search' => true,
+    'used_in_product_listing' => true,
+    'unique' => false,
+    'apply_to' => 'simple',  // Apply to simple product type
+));
+
 
 $installer->endSetup();
